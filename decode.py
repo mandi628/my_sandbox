7bee3c5dccd261607730692e439c53c62e97a9b9
mandi628 <amanda.aumann@gmail.com>
2024-06-29 13:15:43 -0400

    Compile files from devices

---
+++ decode.py
@@ -0,0 +1,4 @@
+# develop a function name 'decode(message_file)'. The 'message_file' argument is a string containing the file path for a .txt file that has an encoded message. When the function is called, it should read the file specified by its argument, decode the message, and return (not print) the decoded message as a string in a specific format,
+    
+# The key to decoding the message is to use the words corresponding to the numbers at the end of each pyramid line (in this example, 1, 3, and 6). The decoded message is formed by taking these words and separating them by individual spaces, with no extra characters. You shoul ignore all the other words.
+
