cc1142914b792fc197b225fa7137ed5eee5ddc8a
mandi628 <amanda.aumann@gmail.com>
2024-07-01 10:14:19 -0400

    Updated code

--- decode.py
+++ decode.py
@@ -1,4 +1,28 @@
-# develop a function name 'decode(message_file)'. The 'message_file' argument is a string containing the file path for a .txt file that has an encoded message. When the function is called, it should read the file specified by its argument, decode the message, and return (not print) the decoded message as a string in a specific format,
+# develop a function name 'decode(message_file)'. The 'message_file' argument is a string
+# containing the file path for a .txt file that has an encoded message. When the function
+# is called, it should read the file specified by its argument, decode the message, and
+# return (not print) the decoded message as a string in a specific format,
     
-# The key to decoding the message is to use the words corresponding to the numbers at the end of each pyramid line (in this example, 1, 3, and 6). The decoded message is formed by taking these words and separating them by individual spaces, with no extra characters. You shoul ignore all the other words.
+# The key to decoding the message is to use the words corresponding to the numbers at the
+# end of each pyramid line (in this example, 1, 3, and 6). The decoded message is formed by
+# taking these words and separating them by individual spaces, with no extra characters. You
+# should ignore all the other words.
+
+decode(message_file):
+	#message filepath lookup
+	if message.startswith(%d):
+		return(words.txt[2:])
+
+msg1 = "  1"
+msg2 = " 2 3"
+msg3 = "4 5 6"
+
+key1 = 3 love
+key2 = 6 computer
+key3 = 2 dogs
+key4 = 4 cats
+key5 = 1 i
+key6 = 5 you
+
+
 
