0b76c06653da9b401c5930ca9bf534de4779bfe4
mandi628 <amanda.aumann@gmail.com>
2024-07-02 23:11:03 -0400

    Practice problems

--- learnpython/ex-final.py
+++ learnpython/ex-final.py
@@ -176,3 +176,47 @@ def name_the_benefits_of_functions():
 
 name_the_benefits_of_functions()
 
+# learnpython.org - Classes and Objects
+# Create two new vehicles called car1 and car2. Set car1 to be a red convertible worth
+# $60,000.00 with a name of Fer, and car2 to be a blue van named Jump worth $10,000.00.
+print("__Classes & Objects__")
+# define the Vehicle class
+class Vehicle():
+	name = ""
+	kind = "car"
+	color = ""
+	value = 100.00
+	def description(self):
+		desc_str = "%s is a %s %s worth $%.2f." % (self.name, self.color, self.kind,
+			self.value)
+		return desc_str
+
+car1 = Vehicle()
+car1.name = "Fer"
+car1.kind = "convertible"
+car1.color = "red"
+car1.value = 60000.00
+
+car2 = Vehicle()
+car2.name = "Jump"
+car2.kind = "van"
+car2.color = "blue"
+car2.value = 10000.00
+
+car3 = Vehicle()
+car3.name = "Masha"
+car3.kind = "sedan"
+car3.color = "black"
+car3.value = 9500.00
+
+car4 = Vehicle()
+car4.name = "Pandora"
+car4.kind = "SUV"
+car4.color = "gray"
+car4.value = 400.00
+
+print(car1.description())
+print(car2.description())
+print(car3.description())
+print(car4.description())
+
