2b5909ea8f27e544962928517ec2e0bb31c8da3f
mandi628 <amanda.aumann@gmail.com>
2024-07-05 19:40:46 -0400

    Update code function

--- decode-v2.py
+++ decode-v2.py
@@ -10,29 +10,26 @@ def cipher():
 cipher_list = cipher()
 print(cipher_list)
 
-def code(cipher_list):
-	if cipher_list != " ":
-		return cipher_list[-2]
+def code():
+	for code in cipher_list:
+		x = 0
+		while x < len(cipher_list):
+			return cipher_list[x][-2]
+		x += 1
 
-code = code(cipher_list)
+code = code()
 print(code)
 
-#def decode(message_file):
-#	for cipher in message_file:
-#		cipher = message_file.readline()
-#		return cipher
-#	for code in cipher:
-#		code = cipher[-2]
-#		return code
-#	for msg in code:
-#		key.get(msg)
-#		return msg
-#
-#key = {'6': 'computers', '3': 'love', '1': 'i'}
-#
-#decode(message_file)
-#
-#close("message_file.txt")
-#
-#print(code)
-#print(msg)
+key = {'6': 'computers', '3': 'love', '1': 'i'}
+
+def decode(message_file):
+	for msg in code:
+		msg = code()
+		key.get(msg)
+		return msg
+
+decode(message_file)
+
+close("message_file.txt")
+
+print(msg)
