eba38c9d55545808d9f1de8aae71b0437663970a
mandi628 <amanda.aumann@gmail.com>
2024-07-05 15:54:33 -0400

    Update decode-v2

--- decode-v2.py
+++ decode-v2.py
@@ -1,20 +1,40 @@
 message_file = open("message_file.txt")
 
-def decode(message_file):
+msg = list()
+
+def cipher():
 	for cipher in message_file:
-		cipher = message_file.readline()
+		cipher = message_file.readlines()
 		return cipher
-	for code in cipher:
-		code = cipher[-2]
-		return code
-	for msg in code:
-		key.get(msg)
-		return msg
 
-key = {'6': 'computers', '3': 'love', '1': 'i'}
-
-decode(message_file)
-
+cipher = cipher()
 print(cipher)
+
+def code():
+	for code in cipher:
+		code_str = cipher
+		code = [cipher[-2]]
+		return code
+
+code = code()
 print(code)
-print(msg)
+
+#def decode(message_file):
+#	for cipher in message_file:
+#		cipher = message_file.readline()
+#		return cipher
+#	for code in cipher:
+#		code = cipher[-2]
+#		return code
+#	for msg in code:
+#		key.get(msg)
+#		return msg
+#
+#key = {'6': 'computers', '3': 'love', '1': 'i'}
+#
+#decode(message_file)
+#
+#close("message_file.txt")
+#
+#print(code)
+#print(msg)
