1f367b1e4a0e676142e72a6b0ee70a3345358fca
mandi628 <amanda.aumann@gmail.com>
2024-07-07 09:28:30 -0400

    Update v2

--- decode-v2.py
+++ decode-v2.py
@@ -7,29 +7,37 @@ def cipher():
 		cipher = message_file.readlines()
 		return cipher
 
-cipher_list = cipher()
-print(cipher_list)
+cipher = cipher()
+print(cipher)
 
 def code():
-	for code in cipher_list:
-		x = 0
-		while x < len(cipher_list):
-			return cipher_list[x][-2]
+	x = 0
+	while x < len(cipher):
+		return cipher[x][-2] #When I use "print" in this line, I get what I'm looking for, but "return" only gives me one reponse.
 		x += 1
 
 code = code()
 print(code)
 
-key = {'6': 'computers', '3': 'love', '1': 'i'}
-
-def decode(message_file):
-	for msg in code:
-		msg = code()
-		key.get(msg)
-		return msg
-
-decode(message_file)
-
-close("message_file.txt")
+msg = {'6': 'computers', '3': 'love', '1': 'i', '4': 'dogs', '2': 'cats'}
 
 print(msg)
+
+msg1 = (code[0])
+msg2 = (code[1])
+msg3 = (code[2])
+#print(msg.get(msg1))
+#print(msg.get(msg2))
+#print(msg.get(msg3))
+
+#def decode(message_file):
+#	for msg in code:
+#		msg = code()
+#		key.get(msg)
+#		return msg
+
+#decode(message_file)
+
+#close("message_file.txt")
+
+#print(msg)
