cbe514345f140f5358543b8e7af4b0bf37776e5c
mandi628 <amanda.aumann@gmail.com>
2024-07-07 15:05:44 -0400

    Update v2

--- decode-v2.py
+++ decode-v2.py
@@ -2,6 +2,8 @@ message_file = open("message_file.txt")
 
 msg = list()
 
+#print(message_file.readlines())
+
 def cipher():
 	for cipher in message_file:
 		cipher = message_file.readlines()
@@ -12,27 +14,28 @@ print(cipher)
 
 def code():
 	x = 0
+	code = []
 	while x < len(cipher):
-		return cipher[x][-2] #When I use "print" in this line, I get what I'm looking for, but "return" only gives me one reponse.
+		code.append(cipher[x][-2])
 		x += 1
 
 code = code()
 print(code)
 
-msg = {'6': 'computers', '3': 'love', '1': 'i', '4': 'dogs', '2': 'cats'}
+#msg = {'6': 'computers', '3': 'love', '1': 'i', '4': 'dogs', '2': 'cats'}
 
-print(msg)
+#print(msg)
 
-msg1 = (code[0])
-msg2 = (code[1])
-msg3 = (code[2])
+#msg1 = (kode[0])
+#msg2 = (kode[1])
+#msg3 = (kode[2])
 #print(msg.get(msg1))
 #print(msg.get(msg2))
 #print(msg.get(msg3))
 
 #def decode(message_file):
-#	for msg in code:
-#		msg = code()
+#	for msg in kode:
+#		msg = kode()
 #		key.get(msg)
 #		return msg
 
