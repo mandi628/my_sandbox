b966046427dd7e1fffc504a5dcfb0e5a11072c9b
mandi628 <amanda.aumann@gmail.com>
2024-07-17 21:18:44 -0400

    "Fixed issue with msg file"

--- decoder/decode-v3.py
+++ decoder/decode-v3.py
@@ -5,7 +5,7 @@ filename = input("Enter the filename you'd like to decode: ")
 file = open(filename)
 message_file = file
 
-msg = {'6': 'computers', '3': 'love', '1': 'I', '4': 'dogs', '5': 'you', '2': 'cats', '7': 'meet', '8': 'elephants', '9': 'parade', '0': 'party', 's': 'Suzi', 't': 'Tess', 'm': 'Micky', 'h': 'Ashleigh', 'a': 'Abbie', 'c': 'Mama'
+msg = {'6': 'computers', '3': 'love', '1': 'I', '4': 'dogs', '5': 'you', '2': 'cats', '7': 'meet', '8': 'elephants', '9': 'parade', '0': 'party', 's': 'Suzi', 't': 'Tess', 'm': 'Micky', 'h': 'Ashleigh', 'a': 'Abbie', 'c': 'Mama', 'x': 'at', 'b': 'this', 'z': 'is', 'd': 'really', 'e': 'cool',
 }
 
 def text():
