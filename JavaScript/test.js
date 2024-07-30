29d2d61255e218105272f3cca4419dd504df5fe2
mandi628 <amanda.aumann@gmail.com>
2024-07-30 12:45:30 -0400

    Upload previous practice projects

---
+++ JavaScript/test.js
@@ -0,0 +1,36 @@
+// let message;
+// message = 'Hello world!';
+// alert(message);
+
+// let hello = 'Hello world!';
+// let message = hello;
+// alert(hello);
+// alert(message);
+
+// let $ = 1;
+// let _ = 2;
+// alert($ + _);
+
+// const COLOR_RED = "#F00";
+// const COLOR_GREEN = "#0F0";
+// const COLOR_BLUE = "#00F";
+// const COLOR_ORANGE = "#FF7F00";
+
+// let color = COLOR_ORANGE;
+// alert(color);
+
+// let Name = 'John';
+// let admin = Name;
+// alert(Name);
+
+// let admin, Name;
+// Name = 'John';
+// admin = Name;
+// alert(Name);
+
+// let ourPlanetName = 'Earth';
+// let currentUserName = 'John';
+
+
+
+// <3 mandi628
