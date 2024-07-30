f298f55b70864c2c37ab65d0373e2399fb7c75ce
mandi628 <mandi628.codes@gmail.com>
2024-07-30 12:44:24 -0400

    Add templates

---
+++ templates/bash-template.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+# Copyright 20__ mandi628
+# E-mail: mandi627.codes@gmail.com
+# License
+
+# Title
+# https://mandi628.github.io/____
+
---
+++ templates/ruby-template.rb
@@ -0,0 +1,8 @@
+#!/usr/bin/env ruby
+# Copyright 20__ mandi628
+# E-mail: mandi627.codes@gmail.com
+# License
+
+# Title
+# https://mandi628.github.io/____
+
