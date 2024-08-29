aa0beeeb31d0f666027288b4c0093618ea514907
mandi628 <mandi628.codes@gmail.com>
2024-08-28 21:07:08 -0400

    Attempt challenges

--- bash/bash-by-example/challenge015.sh
+++ bash/bash-by-example/challenge015.sh
@@ -1,16 +1,20 @@
-#! /usr/bin/env python3
+#! /bin/bash
 
 # Python by Example, by Nichola Lacey
 # borrowed from library 2024.07.05
 
 # Chapter 2: If Statements
 
-print("Challenge 015")
+echo "Challenge 015"
+
 # Ask the user to enter their favourite colour. If they enter "red", "RED" or "Red" display the message
 # "I like red, too", otherwise display the message "I don't like [colour], I prefer red."
-color = input("What is your favorite color? ")
 
-if color == "red" or color == "RED" or color == "Red":
-    print("I like red, too.")
-else:
-    print("I don't like %s, I prefer red." % str.lower(color))
+echo "What is your favorite color?"
+read color
+
+if $color = "red" or $color = "RED" or $color = "Red"
+    echo "I like red, too."
+else
+    echo "I don't like $color, I prefer red."
+fi
